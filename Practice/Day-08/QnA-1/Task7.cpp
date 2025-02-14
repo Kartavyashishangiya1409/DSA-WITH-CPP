a8bf81569f62101a1d140a2f0b3bc53d9ae2a926
kartavyashishangiya1409 <kartavyashishangiya1409@gmail.com>
2025-02-14 17:55:20 +0530

    Update

--- Practice/Day-08/QnA-1/Task7.cpp
+++ Practice/Day-08/QnA-1/Task7.cpp
@@ -10,12 +10,22 @@ using namespace std;
 
 int main(){
 
-    for (char row = 65; row <= 69; row++) 
+    // for (char row = 65; row <= 69; row++) 
+    // {
+    //     for (char col = row; col >= 65 ; col--) 
+    //     {
+    //         cout << col << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    for (char i = 'A'; i <= 'E'; i++)
     {
-        for (char col = row; col >= 65 ; col--) 
+        for (char j = i ; j >= 'A'; j--) 
         {
-            cout << col << " ";
+            cout << j << " ";
         }
         cout << endl;
     }
+    
 }
--- Practice/Day-08/QnA-1/Task8.cpp
+++ Practice/Day-08/QnA-1/Task8.cpp
@@ -10,6 +10,30 @@ using namespace std;
 
 int main(){
 
+    int  count = 1;
+    
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << count << " ";
+            count++;
+        }
+        cout << endl;
+    }
+
+    cout << endl << endl;
+    
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = i; j >= 1; j--)
+        {
+            cout << count << " ";
+            count++;
+        }
+        cout << endl;
+    }
+    
 
     
 }
