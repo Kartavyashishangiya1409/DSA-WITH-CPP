7735c8ba6ce4a8b0c8b494d28579eed6406ea05b
kartavyashishangiya1409 <kartavyashishangiya1409@gmail.com>
2025-02-20 20:58:15 +0530

    Update

--- Practice/Day-09/QnA-1/Q7.cpp
+++ Practice/Day-09/QnA-1/Q7.cpp
@@ -16,11 +16,42 @@ int main(){
         {
             cout << j << " ";
         }
-        // for (int space ; space ; space)
-        // {
-        //     /* code */
-        // }
+        for (int space = i; space < 5 ; space++)
+        {
+            cout << "  ";
+        }
+        for (int space = i; space < 5; space++)
+        {
+            cout << "  ";
+        }
+        for (int j = i; j >= 1; j--)
+        {
+            cout << j << " ";
+        }
         cout << endl;
     }
+
+    // cout << endl;
+    // cout << endl;
+
+    // 5 4 3 2 1
+    //  4 3 2 1
+    //   3 2 1
+    //    2 1
+    //     1
+
+    // for (int i = 5; i >= 1; i--)
+    // {
+    //     for (int space = i; space < 5; space++)
+    //     {
+    //         cout << " ";
+    //     }
+    //     for (int j = i; j >= 1; j--)
+    //     {
+    //         cout << j << " ";
+    //     }
+    //     cout << endl;
+    // }
+    
     
 }
